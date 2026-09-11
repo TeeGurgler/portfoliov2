0fe550a5048c70f26cda6d61deb43684d1c4a1d6
Andrés Jauch <jauch.andres@gmail.com>
2026-09-11 20:03:24 +0200

    cv download fix

--- react-ts/src/components/footer.tsx
+++ react-ts/src/components/footer.tsx
@@ -83,7 +83,7 @@ export function Footer() {
                 asChild
               >
                 <a
-                  href="/cv.pdf"
+                  href="/Andres_Jauch_CV.pdf"
                   download="Andres_Jauch_CV.pdf"
                   target="_blank"
                   rel="noopener noreferrer"
